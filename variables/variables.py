0cea924c9db530d9da3a0d3ebccec0e7df04a482
Jeremias O. Silva <jeremiasprogrammer@gmail.com>
2020-06-09 20:07:51 -0300

    Adicionado arquivo que testa variáveis.

---
+++ variables/variables.py
@@ -0,0 +1,11 @@
+idade = 20
+nome = "Jeremias"
+peso = 80.9
+casado = True 
+
+print(nome, "tem ", idade, "de idade, seu peso é ", peso)
+print(" e ")
+if casado:
+    print("É casado!")
+else:
+    print("Não é casado!")
