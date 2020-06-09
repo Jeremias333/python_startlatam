df0bd6325a3c92c0c5a39e480cc506f23f460e9b
Jeremias O. Silva <jeremiasprogrammer@gmail.com>
2020-06-09 20:06:53 -0300

    Testes iniciais.

---
+++ first/Teste.py
@@ -0,0 +1,9 @@
+Nome =input("qual seu nome")
+idade =input("qual sua idade")
+print("Olá",Nome,"Prazer em conhecer você")
+print(Nome,"Vamos tentar agora uma soma")
+Num1 =input("Escolha um número")
+print("Muito bom")
+num2 =input("Agora escolha outro número")
+
+ 
---
+++ first/odin.py
@@ -0,0 +1,10 @@
+name = input("Digite um nome")
+
+def nameGod(name):
+    if(name == "Loki"):
+        print("The name of god is: "+name)
+    else:
+        print("The name of god is: Another name different than Loki")
+
+
+nameGod(name)
