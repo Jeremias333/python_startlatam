14d544706405361c23167284ced266ea5ff2fb70
Jeremias S <jeremiasprogrammer@gmail.com>
2020-07-08 22:19:10 -0300

    Retirado comentário da função add()

--- data_base/mini_projeto_empresa_chartos/src/main.py
+++ data_base/mini_projeto_empresa_chartos/src/main.py
@@ -4,4 +4,4 @@ from models.funcionario import Funcionario
 funcionario = Funcionario("Jeremias", "Entregador", 1999.00)
 dao_funcionario = DaoFuncionario()
 
-#dao_funcionario.add(funcionario)
+dao_funcionario.add(funcionario)
