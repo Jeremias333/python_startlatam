7a1dbd0141fe74788c2b919bcddb8cfa8acc8926
Jeremias O. Silva <jeremiasprogrammer@gmail.com>
2020-07-10 17:04:02 -0300

    Adicionado arquivos utilitários.

---
+++ data_base/mini_projeto_empresa_chartos/src/utils/rand.py
@@ -0,0 +1,9 @@
+import random
+
+def printRand(init, final):
+	print(random.randint(init, final))
+
+def randInt(init, final):
+	return random.randint(init, final)
+
+printRand(int(input("inicio: ")), int(input("final: ")))
